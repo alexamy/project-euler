d005a85b2485977c63d3d596424071f55eb79865
alexij <achasch@gmail.com>
2017-09-15 23:38:47 +0300

    problem 8

---
+++ Problem8.hs
@@ -0,0 +1,25 @@
+module Problem8 where
+
+import Data.Char
+import DataValues
+
+numberString = DataValues.numberString_pr8
+substrings = sublists
+
+sublists list trunkLen =
+  sublists' list trunkLen []
+  where
+  sublists' list trunkLen acc
+    | trunkLen > length list = acc
+    | otherwise =
+      sublists' (tail list) trunkLen accum
+      where
+      accum = acc ++ [take trunkLen list]
+
+result =
+  maximum $ map productOfDigits digitLists
+  where
+  productOfDigits = foldl1 (*)
+  digitLists = map digitize subs
+  digitize = map digitToInt
+  subs = substrings numberString 13
